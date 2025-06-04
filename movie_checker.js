feb3bf6caf0ecb01b6dec93a3185e201aa46db2f
SaltedGoose <96890090+SaltedGoose@users.noreply.github.com>
2025-06-04 21:46:39 +0100

    Add files via upload

--- movie_checker.js
+++ movie_checker.js
@@ -1,9 +1,1 @@
-var search_button = document.getElementById("search");
-var data = parseCSV
 
-search_button.onclick = search;
-
-function search(){
-    var main = document.getElementById("main");
-    main.innerHTML = data;
-}
