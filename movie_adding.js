29b771974add4fbc667c6874e09bc6d660178e1f
SaltedGoose <96890090+SaltedGoose@users.noreply.github.com>
2025-06-10 16:41:13 +0100

    Update movie_adding.js

--- movie_adding.js
+++ movie_adding.js
@@ -1,12 +1,3 @@
-let addButton = document.getElementById("add")
-
-addButton.onclick = add
-
-async function add(){
-    let url = 'https://api.sheety.co/b28a011384b372aeb5d4b9e2e430e43a/movies/moviesOwned';
-    let response = await fetch(url);
-    let json = await response.json();
-    let movie_list = json.moviesOwned
-
-    
-}
+let addButton=document.getElementById("add")
+addButton.onclick=add
+async function add(){let url='https://api.sheety.co/b28a011384b372aeb5d4b9e2e430e43a/movies/moviesOwned';let response=await fetch(url);let json=await response.json();let movie_list=json.moviesOwned}
