dab614af287aa5d7f3e1c75714ce02a05144ed2a
SaltedGoose <96890090+SaltedGoose@users.noreply.github.com>
2025-06-10 20:43:17 +0100

    Update movie_checker.js

--- movie_checker.js
+++ movie_checker.js
@@ -4,32 +4,30 @@ searchButton.onclick=search
 randomButton.onclick=random
 async function retreiveDatabase(){var url="https://api.sheety.co/b28a011384b372aeb5d4b9e2e430e43a/movies/moviesOwned";var response=await fetch(url);var json=await response.json()
 return json.moviesOwned}
-async function random(){var input=document.querySelector("#search-input")
+async function random(){var input=$("#search-input")
 input.value="";var dataBase=await retreiveDatabase()
 var x=Math.floor((Math.random()*dataBase.length));var movieData=dataBase[x]
 show_movie_info(movieData)}
-async function search(){var input=document.querySelector("#search-input")
-var inputText=input.value
+async function search(){var input=$("#search-input")
+var inputText=input.val()
 var movieData=null
 var dataBase=await retreiveDatabase()
 for(var i=0;i<dataBase.length;i++){if(dataBase[i].movie.toLowerCase()==inputText.toLowerCase()){movieData=dataBase[i]
 break}}
 show_movie_info(movieData,inputText)}
-function show_movie_info(movie_data,inputText){var movieName=document.querySelector("#name")
-var movieLocation=document.querySelector("#location")
-var movieLetter=document.querySelector("#letter")
-var movieNumber=document.querySelector("#number")
-if(movie_data){movieName.innerText="Movie Name: "+movie_data.movie
-movieLocation.innerText="Location: "+movie_data.location
-movieLetter.innerText="Letter: "+movie_data.letter
-movieNumber.innerText="Number: "+movie_data.number}else{alert("Movie: "+inputText+" Not Found. Please check name and try again")
-movieName.innerText="Movie Name: Movie Not Found"
-movieLocation.innerText="Location: "
-movieLetter.innerText="Letter: "
-movieNumber.innerText="Number: "}}
+function show_movie_info(movie_data,inputText){var movieName=$("#name")
+var movieLocation=$("#location")
+var movieLetter=$("#letter")
+var movieNumber=$("#number")
+if(movie_data){movieName.text("Movie Name: "+movie_data.movie)
+movieLocation.text("Location: "+movie_data.location)
+movieLetter.text("Letter: "+movie_data.letter)
+movieNumber.text("Number: "+movie_data.number)}else{alert("Movie: "+inputText+" Not Found. Please check name and try again")
+movieName.text("Movie Name: Movie Not Found")
+movieLocation.text("Location: ")
+movieLetter.text("Letter: ")
+movieNumber.text("Number: ")}}
 function displayMovieImg(){var randomImgNum=Math.floor(Math.random()*10)+1
-var displayImg=document.querySelector("#movie-img")
-displayImg.setAttribute("src","images/img_"+randomImgNum+".png")}
-document.addEventListener("keydown",function(event){console.log(event.key)
-if(event.key==="Enter"){search()}})
-setInterval(displayMovieImg,10000)
+var displayImg=$("#movie-img");displayImg.fadeTo("slow",0,function(){displayImg.attr("src","images/img_"+randomImgNum+".png")});displayImg.fadeTo(5000,100)}
+$(document).keydown(function(event){if(event.key==="Enter"){search()}})
+setInterval(displayMovieImg,2000)
